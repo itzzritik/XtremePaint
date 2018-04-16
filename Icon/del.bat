5a15a71bfa2410f16042be653d80906ee1e654b7
Ritik Srivastava <ritik.space@gmail.com>
2018-04-16 23:54:07 +0530

    edit

--- Icon/del.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rmdir /s /q  "%programfiles%/Ritiks Paint"
-rmdir /s /q  "%HOMEDRIVE%%HOMEPATH%\Start Menu\Programs\New Folder"
-echo y | del  "%userprofile%\Desktop\Ritik's Paint.lnk"
--- userd.java
+++
@@ -1,7 +0,0 @@
-public class userd
-{
-	public static void main()
-	{
-		System.out.println(System.getProperty("user.dir")+"\n"+System.getProperties());
-	}
-}
